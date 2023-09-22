9fc68e0194029084e236b87b33fac4553284bfa4
A VENKATA SIVASANKAR <97689510+AVENKATASIVASANKAR@users.noreply.github.com>
2023-09-22 16:36:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ var timer=setInterval(function(){
 
 
 if (minutes % 60 === 0  && seconds <= 5){
- 
-  audio = new Audio('sound/alarm1.wav');
+  audio = new Audio('sound/alarm1(1).wav');
   audio.play();
   }
       else if(minutes / 30 === 1 && seconds <= 5){
